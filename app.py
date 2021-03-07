f62c59ddd9e02b57be7f546103c7b83ab594c065
Aleks Pawlak <aleks.pawlak@code.berlin>
2021-03-07 20:17:29 +0100

    First commit nav done

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', page_title="kac_wissuwa_industries™")
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html',
+                           page_title="kac_wissuwa_industries™")
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html',
+                           page_title="kac_wissuwa_industries™")
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=8080, debug=True)
